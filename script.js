9073fa283f347d8016b2036cada3bf0bcb1e2121
Eliot Bernaud <43908605+BigCawo@users.noreply.github.com>
2023-11-09 16:40:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
-
 function test(x) {
 
-    
+ 
     
     
 
@@ -10,6 +9,7 @@ function test(x) {
     if ( x == 0){
         y = "dessin"
         z = "buttonDessin"
+        a = "descDessin"
         buttonValue = document.getElementById("buttonDessin").value
 
     }
@@ -17,18 +17,21 @@ function test(x) {
     if ( x == 1){
         y = "objet"
         z = "buttonObjet"
+        a = "descObjet"
         buttonValue = document.getElementById("buttonObjet").value
     }
 
     if ( x == 2){
         y = "fiction"
         z = "buttonFiction"
+        a = "descFiction"
         buttonValue = document.getElementById("buttonFiction").value
     }
     
     if ( x == 3){
         y = "memoire"
         z = "buttonMemoire"
+        a = "descMemoire"
         buttonValue = document.getElementById("buttonMemoire").value
     }
     
@@ -36,6 +39,8 @@ function test(x) {
         if (buttonValue == "off") {
 
             const reset = document.getElementsByClassName("button");
+
+            
             for (let i = 0; i < reset.length; i++) {
                 reset[i].value = "off"
                 reset[i].style.backgroundColor = "rgba(194, 194, 194, 0.486)"
@@ -47,10 +52,24 @@ function test(x) {
             document.getElementById(z).style.backgroundColor = "black"
             document.getElementById(z).style.color = "white"
 
+
             const icons = document.getElementsByClassName("icons");
             for (let i = 0; i < icons.length; i++) {
                 icons[i].style.display = "none";
             }
+            //descriptions
+            /*
+            const resetDesc = document.getElementsByClassName("desc");
+
+            for (let i = 0; i < resetDesc.length; i++) {
+                resetDesc[i].style.maxHeight = "0"
+                
+            }
+ 
+            document.getElementById(a).style.maxHeight = "calc( 80vh - var(--h-sidebar) )"
+            */
+            //descriptions
+
 
             const selection = document.getElementsByClassName(y);
             for (let i = 0; i < selection.length; i++) {
@@ -71,7 +90,15 @@ function test(x) {
             for (let i = 0; i < icons.length; i++) {
                 icons[i].style.display = "inline";
             }
+            //descriptions
+            /*
+            const resetDesc = document.getElementsByClassName("desc");
 
+            for (let i = 0; i < resetDesc.length; i++) {
+                resetDesc[i].style.maxHeight = "0"
+            }
+            */
+            //descriptions
 
         
         }
